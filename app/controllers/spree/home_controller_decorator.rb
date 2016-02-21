0cf853130c2473e6a823f453698243e0948ca897
quentin <quentinuys@gmail.com>
2016-02-21 09:21:05 +0200

    Quick Fix for ordering

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -2,7 +2,7 @@ module Spree
   HomeController.class_eval do
 
     def hot_products
-      @products = Product.where("viewed > ?", 0).order(viewed: :desc).first(20)
+      @products = Product.where("viewed > ?", 0).reorder(viewed: :desc).limit(20)
     end
     
   end
